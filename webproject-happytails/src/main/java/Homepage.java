c06f5d37eda477945c9388df27cb3177787f1c66
Jen Swanson <Jswanson51@unomaha.edu>
2023-11-12 15:47:09 -0600

    Added Homepage Servlet.

---
+++ webproject-happytails/src/main/java/Homepage.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Homepage
+ */
+@WebServlet("/HomePage")
+public class Homepage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Homepage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//Prints the homepage title and motto.
+		response.getWriter().append("<h1 style=\"font-family:verdana\"><center>Happy Tails</center></h1>");
+		response.getWriter().append("<h4 style=\"font-family:verdana\"><center>Where all animals find happy homes!</center></h4>");
+		response.setContentType("text/html");
+	    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
